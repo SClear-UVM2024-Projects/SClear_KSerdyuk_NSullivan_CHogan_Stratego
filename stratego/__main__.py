e8e0c6bbba8ec62129654411ca9495b889303aae
Sam C <97773905+SamC0124@users.noreply.github.com>
2024-02-24 10:21:29 -0500

    Delete outside-parent directory

--- stratego/__main__.py
+++
@@ -1,8 +0,0 @@
-import arcade
-from views import IntroView
-from config import config
-
-window = arcade.Window(config['window']['width'], config['window']['height'], config['window']['title'])
-intro_view = IntroView()
-window.show_view(intro_view)
-arcade.run()
